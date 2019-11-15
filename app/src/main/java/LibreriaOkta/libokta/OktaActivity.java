76743a0f3ffd76250724e20feff9d37be2901a0c
azavala_yunius <azavala.yunius@gmail.com>
2019-11-14 23:13:38 -0600

    session token/access token

--- app/src/main/java/LibreriaOkta/libokta/OktaActivity.java
+++ app/src/main/java/LibreriaOkta/libokta/OktaActivity.java
@@ -1,51 +1,51 @@
-package com.libokta;
+package LibreriaOkta.libokta;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
-import com.dacompsc.general.base.BaseSharedActivity;
+import com.libokta.R;
 
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
 
 import LibreriaOkta.OktaInterface;
 import LibreriaOkta.OktaManager;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
-import com.dacompsc.general.base.BaseSharedActivity;
-
-public class OktaActivity extends BaseSharedActivity implements OktaInterface.View {
+public class OktaActivity extends AppCompatActivity implements OktaInterface.View {
 
     public static final MediaType MEDIA_TYPE = MediaType.parse("application/json");
     private OktaInterface.Presenter mPresenter;
-
+public WebView mView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         mView = (WebView) findViewById(R.id.webView);
         mPresenter = new OktaManager(this);
-        mPresenter.forgetPassword("azavala.yunius@gmail.com","https://dev-575230.okta.com");
+      //  mPresenter.forgetPassword("azavala.yunius@gmail.com","https://dev-575230.okta.com");
+        mPresenter.SingIn("produccioneszavala4@gmail.com","Morado456","https://dev-575230.okta.com","0oa1r27qreeuNSDYU357","0oa1r27qreeuNSDYU357");
+            //mPresenter.changePassword("aide34zavala@gmail.com","https://dev-575230.okta.com","00NzM_yOsJbQDeRlLu0KeoqudoIc_rrN8h-4VGghX6","1234567","12345678");
+        //mPresenter.createUser("Pedro","Marquez","imss","Imss","Mex","Zac","Zac","pedro5@gmail.com","123456","https://dev-575230.okta.com",true,
+          //      true,"00NzM_yOsJbQDeRlLu0KeoqudoIc_rrN8h-4VGghX6","0oa1r27qreeuNSDYU357");
+        //mPresenter.SingIn("pedro5@gmail.com","123456","https://dev-575230.okta.com","0oa1r27qreeuNSDYU357","0oa1r27qreeuNSDYU357");
+//            mPresenter.forgetPassword("aide34zavala@gmail.com","https://dev-575230.okta.com","00NzM_yOsJbQDeRlLu0KeoqudoIc_rrN8h-4VGghX6");
+
     }
 
 
     @Override
     public void resultSingIn(JSONObject result) {
         JSONObject resultado = result;
+        mPresenter.getCode("https://dev-575230.okta.com","0oa1r27qreeuNSDYU357",mView,result);
     }
 
     @Override
     public void resultToken(JSONObject result) {
         JSONObject resultado = result;
+
     }
 
     @Override
@@ -68,9 +68,15 @@ public class OktaActivity extends BaseSharedActivity implements OktaInterface.Vi
         JSONObject resultado = json;
     }
 
+    @Override
+    public void resultchangePasswordNoData(JSONObject result) {
+
+    }
 
     @Override
-    public Toolbar getToolbar() {
-        return null;
+    public void resultPeticion(String code,String codeve) {
+        mPresenter.getTokenWithCode("0oa1r27qreeuNSDYU357","https://dev-575230.okta.com",code,codeve);
     }
+
+
 }
